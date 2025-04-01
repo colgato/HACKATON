2e1dc0618d4cd3ed8870398f6a1d9017e0c11c0d
colgato <149457327+colgato@users.noreply.github.com>
2025-04-01 15:01:45 -0600

    Add files via upload

---
+++ perfil de el tutor/pntalla tutor/pntalla tutorTests/pntalla_tutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  pntalla_tutorTests.swift
+//  pntalla tutorTests
+//
+//  Created by DEVELOP06 on 01/04/25.
+//
+
+import Testing
+@testable import pntalla_tutor
+
+struct pntalla_tutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
